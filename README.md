8b08ecf038f0dac640c955ca58d90a0b79614b0c
Lasse Schmidt <lasse.schmidt@live.de>
2022-02-02 17:55:32 +0100

    make it package ready

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # deep_tabular_augmentation
+
+## How to install
+
+## How to use
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_args = dict(
+    name="deep_tabular_augmentation", 
+    version="0.1.0",
+    author="Lasse Schmidt",
+    author_email="lasse.schmidt@live.de",
+    description="Small package for data augmentation via Deep Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lschmiddey/deep_tabular_augmentation",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
