6d667aad57771cc674df36df4fdb659f0fb27735
Lasse Schmidt <lasse.schmidt@live.de>
2022-11-03 20:24:33 +0100

    make dataloader work on any device

--- deep_tabular_augmentation/dataloaders.py
+++ deep_tabular_augmentation/dataloaders.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data.dataloader import default_collate
 import numpy as np
 
 
@@ -16,6 +17,22 @@ def create_datasets(train_data: np.array, train_target: np.array,
     return trn_ds, tst_ds
 
 
+def create_datasets_with_embs(train_data: np.array, train_target: np.array,
+                              test_data: np.array, test_target: np.array,
+                              train_embs: np.array, test_embs: np.array):
+    """Converts NumPy arrays into PyTorch datasets."""
+
+    trn_ds = TensorDataset(
+        torch.tensor(train_data).float(),
+        torch.tensor(train_embs).float(),
+        torch.tensor(train_target).long())
+    tst_ds = TensorDataset(
+        torch.tensor(test_data).float(),
+        torch.tensor(test_embs).float(),
+        torch.tensor(test_target).long())
+    return trn_ds, tst_ds
+
+
 def create_datasets_no_target_var(train_data: np.array, test_data: np.array):
     """Converts NumPy arrays into PyTorch datasets. No need of any dependend Variable"""
 
@@ -28,15 +45,15 @@ def create_datasets_no_target_var(train_data: np.array, test_data: np.array):
     tst_ds = TensorDataset(
         torch.tensor(test_data).float(),
         torch.tensor(test_target).long())
-    return trn_ds, tst_ds
+    return trn_ds, tst_ds  
+    
 
-
-def create_loaders(data: TensorDataset, bs=128, jobs=0):
+def create_loaders(data: TensorDataset, bs=128, jobs=0, device='cpu'):
     """Wraps the datasets returned by create_datasets function with data loaders."""
 
     trn_ds, tst_ds = data
-    trn_dl = DataLoader(trn_ds, batch_size=bs, shuffle=True, num_workers=jobs)
-    tst_dl = DataLoader(tst_ds, batch_size=bs, shuffle=False, num_workers=jobs)
+    trn_dl = DataLoader(trn_ds, batch_size=bs, shuffle=True, num_workers=jobs, collate_fn=lambda x: tuple(x_.to(device) for x_ in default_collate(x)))
+    tst_dl = DataLoader(tst_ds, batch_size=bs, shuffle=False, num_workers=jobs, collate_fn=lambda x: tuple(x_.to(device) for x_ in default_collate(x)))
 
     return trn_dl, tst_dl
 
